1493b9538b24a026b934a5b7661b5da28b551893
MD Arafath Sarker <arafathmd432@gmail.com>
2026-04-11 18:25:31 +0600

    configuration for production

--- src/_lib/config/db.ts
+++ src/_lib/config/db.ts
@@ -1,14 +1,15 @@
 import { Pool } from "pg";
-declare global{
-    var pgPool:Pool | undefined;
+declare global {
+    var pgPool: Pool | undefined;
 }
 
 
-if(!globalThis.pgPool){
-globalThis.pgPool = new Pool({
-    connectionString:process.env.DB_LINK,
-});
+if (!globalThis.pgPool) {
+    globalThis.pgPool = new Pool({
+        connectionString: process.env.DB_LINK,
+        ssl: { rejectUnauthorized: false },
+    });
 }
 
-const pool:Pool = globalThis.pgPool;
+const pool: Pool = globalThis.pgPool;
 export default pool;
--- src/scripts/dbconfig.ts
+++ src/scripts/dbconfig.ts
@@ -3,12 +3,12 @@ import { tableQueries } from "@/_lib/data/sql";
 import pool from "@/_lib/config/db";
 
 
-;(async ()=>{
- const masterLink = process.env.DB_LINK?.replace(/\/lifestack$/, '/postgres');
-     const masterClient = new Client({ connectionString: masterLink });
-     try {
+; (async () => {
+    const masterLink = process.env.DB_LINK?.replace(/\/lifestack_production$/, '/postgres');
+    const masterClient = new Client({ connectionString: masterLink });
+    try {
         await masterClient.connect();
-        
+
         // Check if medflow exists
         const dbCheck = await masterClient.query(
             "SELECT 1 FROM pg_database WHERE datname = 'lifestack'"
@@ -26,22 +26,21 @@ import pool from "@/_lib/config/db";
         await masterClient.end();
     }
 
-    try{
-          await pool.query("BEGIN");
-          for(let {name,query} of tableQueries) {
-                  const result = await pool.query(query);
-                  if(result.command === "CREATE") console.log(`Table ${name} created.`)
-          }
-         await pool.query("COMMIT");
-         console.log("🎉 All tables synchronized!");
-    }catch(err)
-    {
-         console.log(String(err));
+    try {
+        await pool.query("BEGIN");
+        for (let { name, query } of tableQueries) {
+            const result = await pool.query(query);
+            if (result.command === "CREATE") console.log(`Table ${name} created.`)
+        }
+        await pool.query("COMMIT");
+        console.log("🎉 All tables synchronized!");
+    } catch (err) {
+        console.log(String(err));
         await pool.query("ROLLBACK");
-         process.exit(1);
+        process.exit(1);
     }
-    finally{
-           await pool.end();
-            console.log("Database disconnected");
+    finally {
+        await pool.end();
+        console.log("Database disconnected");
     }
 })();
